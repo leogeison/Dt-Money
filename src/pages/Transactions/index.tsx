af44ff6cdf59c80df4f7054a1ce25c2936dbbdac
Léo Geison <leogeisom@gmail.com>
2024-07-17 22:51:31 -0300

    transaction table creation

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,43 @@
 import { Header } from '../../components/Header'
 import { Summary } from '../../components/Summary'
+import { SearchForm } from './components/SearchForm'
+import {
+  PriceHighLight,
+  TransanctionsContainer,
+  TransanctionsTable,
+} from './styles'
 
 export function Transaction() {
   return (
     <div>
       <Header />
       <Summary />
+
+
+      <TransanctionsContainer>
+      <SearchForm />
+        <TransanctionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighLight variant="income">R$ 12.000,00</PriceHighLight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+
+            <tr>
+              <td width="50%">Hamburguer</td>
+              <td>
+                <PriceHighLight variant="outcome">- R$ 59,00</PriceHighLight>
+              </td>
+              <td>Alimentação</td>
+              <td>10/04/2022</td>
+            </tr>
+          </tbody>
+        </TransanctionsTable>
+      </TransanctionsContainer>
     </div>
   )
 }
