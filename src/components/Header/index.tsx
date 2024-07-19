4e7c8fe1b9c34cf47df7c81cab10b45347c62bd2
Léo Geison <leogeisom@gmail.com>
2024-07-18 23:22:21 -0300

    Creating the modal with radix

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,7 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles'
+import * as Dialog from '@radix-ui/react-dialog'
 import logoImg from '../../assets/logo.svg'
+import { NewTransactionModal } from '../NewTransactionModal'
 
 export function Header() {
   return (
@@ -8,7 +10,12 @@ export function Header() {
         <HeaderContent>
           <img src={logoImg} alt="" />
 
-          <NewTransactionButton>Nova Transação</NewTransactionButton>
+          <Dialog.Root>
+            <Dialog.Trigger asChild>
+              <NewTransactionButton>Nova Transação</NewTransactionButton>
+            </Dialog.Trigger>
+            <NewTransactionModal />
+          </Dialog.Root>
         </HeaderContent>
       </HeaderContainer>
     </div>
